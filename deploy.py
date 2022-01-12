b4ed86f88ce042737c99e06fed825cc4f9d01e37
Yuankai Huo <yuankai.huo@vanderbilt.edu>
2022-01-12 16:59:56 -0600

    update

---
+++ deploy.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('hugo')
+os.system('cd public')
+os.system('git config core.autocrlf true')
+os.system('git add -A')
+os.system('git commit -m "update"')
+os.system('git push origin master')
+os.system('cd ..')
